ced9c5d2208eafc3a391602be35f9a50ab0cf0e2
Ahmed Shaban  Elhdad <ahmed.shaaban.elhdah@gmail.com>
2016-10-18 22:17:54 +0200

    Initial commit

---
+++ app/src/main/java/com/example/ahmedabomazin/tourismguide/Fragment/Help.java
@@ -0,0 +1,19 @@
+package com.example.ahmedabomazin.tourismguide.Fragment;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.ahmedabomazin.tourismguide.R;
+
+public class Help extends Fragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_help,container,false);
+    }
+}
